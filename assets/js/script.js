10b92a31aaf31f4c286130f2eae27e19c9a1d05b
Carolina <c.floresvaldivia@hotmail.com>
2024-06-14 23:12:14 -0400

    ejercicio 3

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function pintar(ele, color = "green") {
+    ele.style.backgroundColor = color;
+}
+const ele = document.getElementById("ele1");
+pintar(ele);
+
+ele.addEventListener("click", function() {
+    pintar(ele, "yellow");
+});
+
+
+
+
+
